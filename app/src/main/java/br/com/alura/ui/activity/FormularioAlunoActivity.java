c9819fc63ce08ab238bc168aa9aeeb01830377c8
Marcos <marcospgd10@gmail.com>
2020-10-19 14:06:16 -0300

    Transferindo dados entre Activities, Econtrando erros com o logcat

--- app/src/main/java/br/com/alura/ui/activity/FormularioAlunoActivity.java
+++ app/src/main/java/br/com/alura/ui/activity/FormularioAlunoActivity.java
@@ -1,11 +1,14 @@
 package br.com.alura.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.Serializable;
 
 import br.com.alura.R;
 import br.com.alura.dao.AlunoDAO;
@@ -26,6 +29,12 @@ public class FormularioAlunoActivity extends AppCompatActivity {
         setTitle(TITULO_APPBAR);
         inicializacaoDosCampos();
         configuraBotaoSalvar();
+
+        Intent dados = getIntent();
+        Aluno aluno = (Aluno) dados.getSerializableExtra("aluno");
+        campoNome.setText(aluno.getNome());
+        campoTelefone.setText(aluno.getTelefone());
+        campoEmail.setText(aluno.getEmail());
     }
 
     private void configuraBotaoSalvar() {
--- app/src/main/java/br/com/alura/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/ui/activity/ListaAlunosActivity.java
@@ -2,7 +2,9 @@ package br.com.alura.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -11,8 +13,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.List;
+
 import br.com.alura.R;
 import br.com.alura.dao.AlunoDAO;
+import br.com.alura.model.Aluno;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -25,6 +30,8 @@ public class ListaAlunosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista_alunos);
         setTitle(TITULO_APPBAR);
         configuraFabNovoAluno();
+        dao.salvar(new Aluno("Marcos","123","marcos@ufv.br"));
+        dao.salvar(new Aluno("Juliana","321","juliana@ufv.br"));
     }
 
     private void configuraFabNovoAluno() {
@@ -49,9 +56,19 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     private void configuraLista() {
         ListView listaDeAlunos = findViewById(R.id.activity_lista_alunos_listview);
+        final List<Aluno> alunos = dao.todos();
         listaDeAlunos.setAdapter(new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
-                dao.todos()));
+               alunos));
+        listaDeAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Aluno alunoEscolhido = alunos.get(position);
+                Intent vaiParaFormularioActivity = new Intent(ListaAlunosActivity.this, FormularioAlunoActivity.class);
+                vaiParaFormularioActivity.putExtra("aluno",alunoEscolhido);
+                startActivity(vaiParaFormularioActivity);
+            }
+        });
     }
 }
